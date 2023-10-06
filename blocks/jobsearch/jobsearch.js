d0c3fc3c0e69a8eed47aeeec7a496a80566b169a
Christoph Loeffler <cloeffle@adobe.com>
2023-10-06 22:20:12 +0200

    First try

---
+++ blocks/jobsearch/jobsearch.js
@@ -0,0 +1,27 @@
+const blocks = document.getElementsByClassName("jobsearch block");
+
+
+for (let block of blocks) {
+    const div = document.createElement("div");
+    div.classList.add("formWrapper");
+   
+    const divs = block.querySelectorAll('div');
+    console.log(divs); 
+
+    div.appendChild(divs[3]);
+    div.appendChild(divs[5]);
+    const inputa = document.createElement("input");
+    div.appendChild(inputa);
+    div.appendChild(divs[7]);
+    const inputb = document.createElement("input");
+    div.appendChild(inputb);
+    div.appendChild(divs[9]);
+    const link = document.createElement("p");
+    link.innerHTML = "Jobs bei Hays intern"
+    div.appendChild(link);
+    
+    //block.appendChild(div);
+    block.insertBefore(div, block.children[1]);
+}
+
+alert("ok");
